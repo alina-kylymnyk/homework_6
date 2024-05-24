193c0a3568dab3f73571d1a0ec263d8409c3b3a1
alina-kylymnyk <kylymnykalina111@gmail.com>
2024-05-24 23:48:58 +0300

    Initial commit

---
+++ homework_6_1.py
@@ -0,0 +1,111 @@
+from collections import UserDict
+import re
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Ім'я не може бути пустим")
+        super().__init__(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not re.match(r'^\d{10}$', value):
+            raise ValueError("Номер телефону має містити 10 символів")
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def delete_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, phone in enumerate(self.phones):
+            if phone.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                return
+        raise ValueError(f"Нномер телефону {old_phone} не знайдено")
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phones_str = '; '.join(p.value for p in self.phones)
+        return f"Ім'я контакту: {self.name.value}, телефон: {phones_str}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            raise ValueError(f"Record with name {name} not found")
+
+
+book = AddressBook()
+
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+book.add_record(john_record)
+
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+for name, record in book.data.items():
+    print(record)
+
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)
+
+found_phone = john.find_phone("5555555555")
+print(f"{john.name.value}: {found_phone}")
+book.delete("Jane")
+
+
+
+
+# Сутності:
+# Field: Базовий клас для полів запису.
+# Name: Клас для зберігання імені контакту. Обов'язкове поле.
+# Phone: Клас для зберігання номера телефону. Має валідацію формату (10 цифр).
+# Record: Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів.
+# AddressBook: Клас для зберігання та управління записами.
+#
+# Функціональність:
+# AddressBook:Додавання записів.
+# Пошук записів за іменем.
+# Видалення записів за іменем.
+# Record:Додавання телефонів.
+# Видалення телефонів.
+# Редагування телефонів.
+# Пошук телефону.
